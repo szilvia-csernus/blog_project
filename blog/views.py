a3c4c96d91ab398954f40b6ba12d0d1e1b0cf4e0
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-02 18:55:21 +0100

    improve post details page

--- blog/views.py
+++ blog/views.py
@@ -1,89 +1,18 @@
-from datetime import date
+from .models import Post, Author, Tag
 
-from django.shortcuts import render
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains1",
-        "image": "mountains.jpg",
-        "author": "Maximilian Schwarzmüller",
-        "date": date(2023, 5, 1),
-        "title": "Mountain Hiking",
-        "excerpt": """There's nothing like the views you get when hiking in
-            the mountains! And I wasn't even prepared for what happened 
-            halfway through the hike...""",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Ducimus accusantium eligendi illo quam autem laborum
-            cumque error debitis quis, aliquam ex eius modi rem aut 
-            ratione nulla tempora dolorem blanditiis?
-            """
-    },
-    {
-        "slug": "nature-at-its-best",
-        "image": "woods.jpg",
-        "author": "Maximilian Schwarzmüller",
-        "date": date(2023, 8, 1),
-        "title": "Nature at its best",
-        "excerpt": """There's nothing like the views you get when hiking in
-            the forest! And I wasn't even prepared for what happened 
-            halfway through the hike...""",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Ducimus accusantium eligendi illo quam autem laborum
-            cumque error debitis quis, aliquam ex eius modi rem aut 
-            ratione nulla tempora dolorem blanditiis?
-            """
-    },
-    {
-        "slug": "progreamming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian Schwarzmüller",
-        "date": date(2023, 7, 1),
-        "title": "Latest on AI",
-        "excerpt": """This week brought yet another set of exciting news
-        on the AI front...""",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Ducimus accusantium eligendi illo quam autem laborum
-            cumque error debitis quis, aliquam ex eius modi rem aut 
-            ratione nulla tempora dolorem blanditiis?
-            """
-    },
-    {
-        "slug": "beach",
-        "image": "beach.jpg",
-        "author": "Maximilian Schwarzmüller",
-        "date": date(2023, 6, 1),
-        "title": "Beach Adventure",
-        "excerpt": """There's nothing like the views you get when strolling on
-        the beach! And I wasn't even prepared for what happened 
-            halfway through the hike...""",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Ducimus accusantium eligendi illo quam autem laborum
-            cumque error debitis quis, aliquam ex eius modi rem aut 
-            ratione nulla tempora dolorem blanditiis?
-            """
-    }
-]
-
-def get_date(post):
-    return post['date']
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+     # [:3] means first 3, [-3:] is not supported!!
+    posts = Post.objects.all().order_by('-date')[:3]
     return render(request, "blog/index.html", {
-        "posts": latest_posts
+        "posts": posts
     })
 
 
 def posts(request):
-    posts = sorted(all_posts, key=get_date)
+    posts = Post.objects.all().order_by('-date')
     return render(request, "blog/all-posts.html", {
         "posts": posts
     }
@@ -91,7 +20,8 @@ def posts(request):
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
