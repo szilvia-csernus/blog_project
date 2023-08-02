2b3bf6eb83d30b10e99d3fa7975f2a3612cf7e6d
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-02 17:21:24 +0100

    add models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post, Author, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",) # Tuple!
+    list_display = ("title", "date", "author",) # Tuple!
+    prepopulated_fields = {"slug": ("title",)} 
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
