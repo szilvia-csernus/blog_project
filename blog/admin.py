c9a2055aa69b8adf92370c1146e14eaa7a0b4b67
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-04 17:05:31 +0100

    add comments to admin and read later button

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date",) # Tuple!
     list_display = ("title", "date", "author",) # Tuple!
     prepopulated_fields = {"slug": ("title",)} 
+    
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post", "text",) # Tuple!
+    
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
